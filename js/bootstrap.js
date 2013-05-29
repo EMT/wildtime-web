c87316244cd406ce0d968562c9d0723450143278
Really Simple <andy@reallysimpleworks.com>
2013-05-29 12:51:26 +0100

    Added JS API functions.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -5,11 +5,42 @@
 $(function(){
 	
 	
-
+	
 	
 	
 });
 
 
+
+var wildtime = {
+
+	loadTimefraames: functions() {
+		var callback = function() {
+			
+		};
+		getTimeframes(callback);
+	},
+	
+	loadTimefraames: functions(timeframe_id) {
+		var callback = function() {
+			
+		};
+		getActivities(timeframe_id, callback);
+	},
+	
+	getTimeframes: function(callback) {
+		$.getJSON('http://wtapi.madebyfieldwork.com/timeframes.json?callback=?', function(data) {
+			callback(data);
+		});
+	},
+	
+	getActivities: function(timeframe_id, callback) {
+		$.getJSON('http://wtapi.madebyfieldwork.com/timeframes/' 
+				+ timeframe_id + '/activities.json?callback=?', function(data) {
+			callback(data);
+		});
+	}
+	
+}
 
 
